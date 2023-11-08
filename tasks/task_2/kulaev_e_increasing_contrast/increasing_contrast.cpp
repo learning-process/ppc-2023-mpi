6a7d07f10604bdb0c3956c30b6f8df5b77646039
Zhenya_Kulaev <kylaev11@gmail.com>
2023-11-08 22:09:04 +0300

    update

--- tasks/task_2/kulaev_e_increasing_contrast/increasing_contrast.cpp
+++ tasks/task_2/kulaev_e_increasing_contrast/increasing_contrast.cpp
@@ -1,8 +1,8 @@
 // Copyright 2023 Kulaev Eugene
 
-#include "task_2/kulaev_e_contrast_enhancement/contrast_enha.h"
+#include "task_2/kulaev_e_increasing_contrast/increasing_contrast.h"
 
-void Seq_linearStretchingHistogram(std::vector<int>* local_img, int global_min,
+void Stretching(std::vector<int>* local_img, int global_min,
                     int global_max, int new_min, int new_max) {
     if (global_min == global_max || local_img->size() == 0) return;
     for (auto& pixel : *local_img) {
@@ -11,7 +11,15 @@ void Seq_linearStretchingHistogram(std::vector<int>* local_img, int global_min,
     }
 }
 
-void Par_linearStretchingHistogram(std::vector<int>* image, int new_min, int new_max, int n, int m) {
+int SeqmetionMax(std::vector<int>* vec) {
+    return *std::max_element(vec->begin(), vec->end());
+}
+
+int SeqmetionMin(std::vector<int>* vec) {
+    return *std::min_element(vec->begin(), vec->end());
+}
+
+void ParallelStretching(std::vector<int>* image, int new_min, int new_max) {
     boost::mpi::communicator comm;
 
     int rankProc = comm.rank();
@@ -19,15 +27,15 @@ void Par_linearStretchingHistogram(std::vector<int>* image, int new_min, int new
 
     int imageSize = image->size();
 
-    if (imageSize == 0 || imageSize < n * m) return;
+    if (imageSize == 0) return;
 
     int chunk_size  = imageSize / numProc;
     int remainder = imageSize % numProc;
 
-    std::vector<int> recv_counts(numProc, chunk_size);
-    std::vector<int> dis_pls(numProc, 0);
+    std::vector<int> recv_counts(numProc);
+    std::vector<int> dis_pls(numProc);
 
-     for (int i = 0; i < numProc; i++) {
+    for (int i = 0; i < numProc; i++) {
         recv_counts[i] = chunk_size;
         if (i < remainder) {
             recv_counts[i]++;
@@ -35,25 +43,23 @@ void Par_linearStretchingHistogram(std::vector<int>* image, int new_min, int new
         dis_pls[i] = (i > 0) ? (dis_pls[i - 1] + recv_counts[i - 1]) : 0;
     }
 
-    int globalMin = 255;
-    int globalMax = 0;
-
     std::vector<int> local_img(recv_counts[rankProc]);
-
     boost::mpi::scatterv(comm, image->data(), recv_counts, dis_pls, local_img.data(), recv_counts[rankProc], 0);
 
     int localMin = 255;
     int localMax = 0;
 
     if (rankProc < image->size()) {
-        localMin = *std::min_element(local_img.begin(), local_img.end());
-        localMax = *std::max_element(local_img.begin(), local_img.end());
+        localMin = SeqmetionMin(&local_img);
+        localMax = SeqmetionMax(&local_img);
     }
 
-    boost::mpi::all_reduce(comm, localMin, globalMin, boost::mpi::minimum<uint8_t>());
-    boost::mpi::all_reduce(comm, localMax, globalMax, boost::mpi::maximum<uint8_t>());
+    int globalMin = 255;
+    int globalMax = 0;
+    boost::mpi::all_reduce(comm, localMin, globalMin, boost::mpi::minimum<int>());
+    boost::mpi::all_reduce(comm, localMax, globalMax, boost::mpi::maximum<int>());
 
-    Seq_linearStretchingHistogram(&local_img, globalMin, globalMax, new_min, new_max);
+    Stretching(&local_img, globalMin, globalMax, new_min, new_max);
 
     boost::mpi::gatherv(comm, local_img.data(), recv_counts[rankProc], image->data(), recv_counts, dis_pls, 0);
 }
