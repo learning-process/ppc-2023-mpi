29289f24064e722c14dc2fb5451b016d5b1d3fd7
Zhenya_Kulaev <kylaev11@gmail.com>
2023-11-09 15:22:41 +0300

    optimization and check style

--- tasks/task_2/kulaev_e_increasing_contrast/increasing_contrast.cpp
+++ tasks/task_2/kulaev_e_increasing_contrast/increasing_contrast.cpp
@@ -2,8 +2,8 @@
 
 #include "task_2/kulaev_e_increasing_contrast/increasing_contrast.h"
 
-void Stretching(std::vector<int>* local_img, int global_min,
-                    int global_max, int new_min, int new_max) {
+void Stretching(std::vector<uint8_t>* local_img, uint8_t global_min,
+                    uint8_t global_max, uint8_t new_min, uint8_t new_max) {
     if (global_min == global_max || local_img->size() == 0) return;
     for (auto& pixel : *local_img) {
         pixel = boost::algorithm::clamp(pixel, global_min, global_max);
@@ -11,15 +11,21 @@ void Stretching(std::vector<int>* local_img, int global_min,
     }
 }
 
-int SeqmetionMax(std::vector<int>* vec) {
+uint8_t generateRandomNumbers(uint8_t min, uint8_t max) {
+    uint8_t randomNumber = min + (std::rand() % (max - min + 1));
+
+    return randomNumber;
+}
+
+uint8_t SeqmetionMax(std::vector<uint8_t>* vec) {
     return *std::max_element(vec->begin(), vec->end());
 }
 
-int SeqmetionMin(std::vector<int>* vec) {
+uint8_t SeqmetionMin(std::vector<uint8_t>* vec) {
     return *std::min_element(vec->begin(), vec->end());
 }
 
-void ParallelStretching(std::vector<int>* image, int new_min, int new_max) {
+void ParallelStretching(std::vector<uint8_t>* image, uint8_t new_min, uint8_t new_max) {
     boost::mpi::communicator comm;
 
     int rankProc = comm.rank();
@@ -29,7 +35,7 @@ void ParallelStretching(std::vector<int>* image, int new_min, int new_max) {
 
     if (imageSize == 0) return;
 
-    int chunk_size  = imageSize / numProc;
+    int chunk_size = imageSize / numProc;
     int remainder = imageSize % numProc;
 
     std::vector<int> recv_counts(numProc);
@@ -43,29 +49,23 @@ void ParallelStretching(std::vector<int>* image, int new_min, int new_max) {
         dis_pls[i] = (i > 0) ? (dis_pls[i - 1] + recv_counts[i - 1]) : 0;
     }
 
-    std::vector<int> local_img(recv_counts[rankProc]);
+    std::vector<uint8_t> local_img(recv_counts[rankProc]);
     boost::mpi::scatterv(comm, image->data(), recv_counts, dis_pls, local_img.data(), recv_counts[rankProc], 0);
 
-    int localMin = 255;
-    int localMax = 0;
+    uint8_t localMin = 255;
+    uint8_t localMax = 0;
 
     if (rankProc < image->size()) {
         localMin = SeqmetionMin(&local_img);
         localMax = SeqmetionMax(&local_img);
     }
 
-    int globalMin = 255;
-    int globalMax = 0;
-    boost::mpi::all_reduce(comm, localMin, globalMin, boost::mpi::minimum<int>());
-    boost::mpi::all_reduce(comm, localMax, globalMax, boost::mpi::maximum<int>());
+    uint8_t globalMin = 255;
+    uint8_t globalMax = 0;
+    boost::mpi::all_reduce(comm, localMin, globalMin, boost::mpi::minimum<uint8_t>());
+    boost::mpi::all_reduce(comm, localMax, globalMax, boost::mpi::maximum<uint8_t>());
 
     Stretching(&local_img, globalMin, globalMax, new_min, new_max);
 
     boost::mpi::gatherv(comm, local_img.data(), recv_counts[rankProc], image->data(), recv_counts, dis_pls, 0);
-}
-
-int generateRandomNumbers(int min, int max) {
-    int randomNumber = min + (std::rand() % (max - min + 1));
-
-    return randomNumber;
 }
