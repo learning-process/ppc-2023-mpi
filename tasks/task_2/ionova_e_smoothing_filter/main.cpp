33af858e8cd194316400b0fba6fef0192e3c1c77
IonovvA <e.i.ionovva@gmail.com>
2023-11-20 20:51:50 +0300

    make tests

--- tasks/task_2/ionova_e_smoothing_filter/main.cpp
+++ tasks/task_2/ionova_e_smoothing_filter/main.cpp
@@ -3,11 +3,102 @@
 
 #include "./filter.h"
 
-TEST(TESTS, Test_1) { ASSERT_EQ(1, 1); }
-TEST(TESTS, Test_2) { ASSERT_EQ(1, 1); }
-TEST(TESTS, Test_3) { ASSERT_EQ(1, 1); }
-TEST(TESTS, Test_4) { ASSERT_EQ(1, 1); }
-TEST(TESTS, Test_5) { ASSERT_EQ(1, 1); }
+TEST(TESTS, Test_1) {
+  int rank = 0;
+  MPI_Comm comm = MPI_COMM_WORLD;
+  MPI_Comm_rank(comm, &rank);
+
+  int rows = 1;
+  int cols = 1;
+  int kernel = 3;
+  std::vector<uint8_t> img{255};
+
+  auto par_result = parFilter(img, rows, cols, kernel, comm);
+
+  if (rank == 0) {
+    auto seq_result = seqFilter(img, rows, cols, kernel);
+    ASSERT_EQ(seq_result, par_result);
+  }
+}
+
+TEST(TESTS, Test_2) {
+  int rank = 0;
+  MPI_Comm comm = MPI_COMM_WORLD;
+  MPI_Comm_rank(comm, &rank);
+
+  int rows = 2;
+  int cols = 2;
+  int kernel = 3;
+  std::vector<uint8_t> img{7, 255, 0, 27};
+
+  auto par_result = parFilter(img, rows, cols, kernel, comm);
+
+  if (rank == 0) {
+    auto seq_result = seqFilter(img, rows, cols, kernel);
+    ASSERT_EQ(seq_result, par_result);
+  }
+}
+
+TEST(TESTS, Test_4) {
+  int rank = 0;
+  MPI_Comm comm = MPI_COMM_WORLD;
+  MPI_Comm_rank(comm, &rank);
+
+  int rows = 4;
+  int cols = 4;
+  int kernel = 3;
+  std::vector<uint8_t> img{150, 30, 100, 30,  20,  7, 0,  0,
+                           255, 55, 0,   255, 124, 0, 27, 6};
+
+  auto par_result = parFilter(img, rows, cols, kernel, comm);
+
+  if (rank == 0) {
+    auto seq_result = seqFilter(img, rows, cols, kernel);
+    ASSERT_EQ(seq_result, par_result);
+  }
+}
+
+TEST(TESTS, Test_empty) {
+  int rank = 0;
+  MPI_Comm comm = MPI_COMM_WORLD;
+  MPI_Comm_rank(comm, &rank);
+
+  int rows = 0;
+  int cols = 0;
+  int kernel = 3;
+  std::vector<uint8_t> img;
+
+  auto par_result = parFilter(img, rows, cols, kernel, comm);
+
+  if (rank == 0) {
+    auto seq_result = seqFilter(img, rows, cols, kernel);
+    ASSERT_EQ(seq_result, par_result);
+  }
+}
+
+TEST(TESTS, Test_random) {
+  int rank = 0;
+  MPI_Comm comm = MPI_COMM_WORLD;
+  MPI_Comm_rank(comm, &rank);
+
+  int rows = 1000;
+  int cols = 1000;
+  int kernel = 3;
+  std::vector<uint8_t> img(rows * cols);
+
+  std::random_device rd;
+  std::mt19937 gen(rd());
+  std::uniform_int_distribution<int> dist(0, 255);
+
+  for (auto& pix : img) pix = dist(gen);
+
+  auto par_result = parFilter(img, rows, cols, kernel, comm);
+
+  if (rank == 0) {
+    auto seq_result = seqFilter(img, rows, cols, kernel);
+    ASSERT_EQ(seq_result, par_result);
+  }
+}
 
 int main(int argc, char** argv) {
   int result_code = 0;
