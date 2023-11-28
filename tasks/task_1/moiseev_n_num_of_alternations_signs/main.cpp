2e9cf70c8a533fc745b340176f589f8964b8c6df
Instmois <nikita-moiseev-2003@ya.ru>
2023-11-28 16:16:13 +0300

    fix

--- tasks/task_1/moiseev_n_num_of_alternations_signs/main.cpp
+++ tasks/task_1/moiseev_n_num_of_alternations_signs/main.cpp
@@ -5,10 +5,8 @@
 TEST(MPI_TESTS, Test_int) {
     int rank = 0;
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-
     std::vector<int> arr{ 1, -2, 3, -4, 5 , -6 };
     int result_par = par_num_of_alternations(arr);
-
     if (rank == 0) {
         int result_seq = seq_num_of_alternations(arr);
         ASSERT_EQ(result_par, result_seq);
@@ -19,10 +17,8 @@ TEST(MPI_TESTS, Test_int) {
 TEST(MPI_TESTS, Test_plus) {
     int rank = 0;
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-
     std::vector<int> arr{ 1, 2, 3, 4, 5 };
     int result_par = par_num_of_alternations(arr);
-
     if (rank == 0) {
         int result_seq = seq_num_of_alternations(arr);
         ASSERT_EQ(result_par, result_seq);
@@ -32,10 +28,8 @@ TEST(MPI_TESTS, Test_plus) {
 TEST(MPI_TESTS, Test_minus) {
     int rank = 0;
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-
     std::vector<int> arr{ -1, -2, -3, -4, -5 };
     int result_par = par_num_of_alternations(arr);
-
     if (rank == 0) {
         int result_seq = seq_num_of_alternations(arr);
         ASSERT_EQ(result_par, result_seq);
@@ -45,10 +39,8 @@ TEST(MPI_TESTS, Test_minus) {
 TEST(MPI_TESTS, Test_null) {
     int rank = 0;
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-
     std::vector<int> arr{ };
     int result_par = par_num_of_alternations(arr);
-
     if (rank == 0) {
         int result_seq = seq_num_of_alternations(arr);
         ASSERT_EQ(result_par, result_seq);
@@ -58,14 +50,10 @@ TEST(MPI_TESTS, Test_null) {
 TEST(MPI_TESTS, Test_random) {
     int rank = 0;
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-
-    const size_t sizeArr = 1'000;
+    const size_t sizeArr = 100;
     std::vector<int> arr;
-    if (rank == 0) {
-        arr = create_random_array(sizeArr);
-    }
+    arr = create_random_array(sizeArr);
     int result_par = par_num_of_alternations(arr);
-
     if (rank == 0) {
         int result_seq = seq_num_of_alternations(arr);
         ASSERT_EQ(result_par, result_seq);
@@ -75,7 +63,6 @@ TEST(MPI_TESTS, Test_random) {
 
 int main(int argc, char** argv) {
     int result_code = 0;
-
     ::testing::InitGoogleTest(&argc, argv);
     ::testing::TestEventListeners& listeners =
         ::testing::UnitTest::GetInstance()->listeners();
--- tasks/task_1/moiseev_n_num_of_alternations_signs/num_of_alternations_signs.cpp
+++ tasks/task_1/moiseev_n_num_of_alternations_signs/num_of_alternations_signs.cpp
@@ -1,30 +1,26 @@
 // Copyright 2023 Moiseev Nikita
 #include <mpi.h>
 #include <vector>
-#include <string>
 #include <iostream>
 #include "task_1/moiseev_n_num_of_alternations_signs/num_of_alternations_signs.h"
 
 std::vector<int> create_random_array(size_t size) {
     std::random_device rd;
     std::mt19937 gen(rd());
-    const int low = -9'999;
-    const int up = 9'999;
+    const int low = -999;
+    const int up = 999;
     std::uniform_int_distribution<int> dist(low, up);
-
     std::vector<int> arr(size);
     for (auto& elem : arr) elem = dist(gen);
-
     return arr;
 }
 
 int seq_num_of_alternations(const std::vector<int>& arr) {
     size_t size = arr.size();
     int num_alternations = 0;
-
     if (size >= 2) {
         for (size_t i = 0; i < size - 1; ++i) {
-            if ((arr[i] > 0 && arr[i + 1] < 0) || (arr[i] < 0 && arr[i + 1] > 0)) {
+            if ((arr[i] * arr[i + 1]) < 0) {
                 num_alternations++;
             }
         }
@@ -34,24 +30,30 @@ int seq_num_of_alternations(const std::vector<int>& arr) {
 
 int par_num_of_alternations(const std::vector<int>& arr) {
     int size_world, rank;
-    size_t size_arr = arr.size();
-    if (size_arr < 2) return 0;
     MPI_Comm_size(MPI_COMM_WORLD, &size_world);
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-    std::vector<int> arr_copy;
-    if (rank == 0) {
-        arr_copy = arr;
+    size_t size_arr = arr.size();
+    if (size_arr < 2) return 0;
+    int block_size = size_arr / size_world;
+    std::vector<int> counts_vector(size_world, block_size);
+    std::vector<int> move_vector(size_world);
+    int end = size_arr % size_world;
+    for (int i = 0; i < end; ++i) {
+        counts_vector[i] = block_size + 1;
     }
-    MPI_Bcast(&size_arr, 1, MPI_UNSIGNED, 0, MPI_COMM_WORLD);
-    arr_copy.resize(size_arr);
-    MPI_Bcast(arr_copy.data(), static_cast<int>(size_arr), MPI_INT, 0, MPI_COMM_WORLD);
-    int local_count = 0;
-    for (size_t i = rank; i < size_arr - 1; i += size_world) {
-        if ((arr_copy[i] > 0 && arr_copy[i + 1] < 0) || (arr_copy[i] < 0 && arr_copy[i + 1] > 0)) {
-            local_count++;
-        }
+    move_vector[0] = 0;
+    for (size_t i = 1; i < counts_vector.size(); ++i) {
+        move_vector[i] = move_vector[i - 1] + counts_vector[i - 1];
     }
-    int global_count = 0;
-    MPI_Allreduce(&local_count, &global_count, 1, MPI_INT, MPI_SUM, MPI_COMM_WORLD);
-    return global_count;
+    for (int i = 0; i < size_world - 1; i++) {
+        if (counts_vector[i + 1] == 0) break;
+        counts_vector[i]++;
+    }
+    std::vector<int> arr_copy(counts_vector[rank]);
+    MPI_Scatterv(arr.data(), counts_vector.data(), move_vector.data(), MPI_INT,
+        arr_copy.data(), counts_vector[rank], MPI_INT, 0, MPI_COMM_WORLD);
+    int local_alternations_count = seq_num_of_alternations(arr_copy);
+    int global_alternations_count;
+    MPI_Allreduce(&local_alternations_count, &global_alternations_count, 1, MPI_INT, MPI_SUM, MPI_COMM_WORLD);
+    return global_alternations_count;
 }
