75ddff4708312bb9dcbac94f142e3931f7d2a8d2
AgentV0 <Vlad58009@gmail.com>
2023-11-29 19:27:48 +0300

    lab1_fix

--- tasks/task_1/ryabkov_v_num_of_alternations_signs/alter_sign.h
+++ tasks/task_1/ryabkov_v_num_of_alternations_signs/alter_sign.h
@@ -4,12 +4,12 @@
 #ifndef TASKS_TASK_1_RYABKOV_V_NUM_OF_ALTERNATIONS_SIGNS_ALTER_SIGN_H_
 #define TASKS_TASK_1_RYABKOV_V_NUM_OF_ALTERNATIONS_SIGNS_ALTER_SIGN_H_
 
-#include <vector>
 #include <mpi.h>
+#include <vector>
 
 void RandVec(int* V, int n);
 int SerialSum(const int* V, int n);
 int ParallelSum(const int* V, int n);
 
 
-#endif  //  TASKS_TASK_1_RYABKOV_V_NUM_OF_ALTERNATIONS_SIGNS_ALTER_SIGN_H
+#endif  // TASKS_TASK_1_RYABKOV_V_NUM_OF_ALTERNATIONS_SIGNS_ALTER_SIGN_H_
