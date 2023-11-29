863ad307a5f01e65a4fae407dabbc903014de62b
katrin000 <katya.videneva.03@mail.ru>
2023-11-29 21:05:36 +0300

    fix bags

--- tasks/task_1/videneva_e_vector_dot_product/vector_dot_product.h
+++ tasks/task_1/videneva_e_vector_dot_product/vector_dot_product.h
@@ -1,6 +1,6 @@
 // Copyright 2023 Videneva Ekaterina
-#ifndef TASKS_TASK_1_VIDENEVA_VECTOR_DOT_PRODUCT_VECTOR_DOT_PRODUCT_H_
-#define TASKS_TASK_1_VIDENEVA_VECTOR_DOT_PRODUCT_VECTOR_DOT_PRODUCT_H_
+#ifndef TASKS_TASK_1_VIDENEVA_E_VECTOR_DOT_PRODUCT_VECTOR_DOT_PRODUCT_H_
+#define TASKS_TASK_1_VIDENEVA_E_VECTOR_DOT_PRODUCT_VECTOR_DOT_PRODUCT_H_
 
 #include <mpi.h>
 #include <vector>
@@ -14,4 +14,4 @@ int scalar_product(const std::vector<int>& vec1, const std::vector<int>& vec2);
 int Parall_product(const std::vector<int>& vec1, const std::vector<int>& vec2);
 
 
-#endif  // TASKS_TASK_1_VIDENEVA_VECTOR_DOT_PRODUCT_VECTOR_DOT_PRODUCT_H_
+#endif //TASKS_TASK_1_VIDENEVA_E_VECTOR_DOT_PRODUCT_VECTOR_DOT_PRODUCT_H_
