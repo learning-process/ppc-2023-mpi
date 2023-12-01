2f8700aa17683f89ea224c2eb98ff11fb64db43e
Savotina <lerasavotina@gmail.com>
2023-12-02 02:52:52 +0300

    release

--- tasks/task_3/savotina_v_configure_gradient/main.cpp
+++ tasks/task_3/savotina_v_configure_gradient/main.cpp
@@ -2,12 +2,82 @@
 #include <gtest/gtest.h>
 
 #include "./par_configure_gradient.h"
+#include "./seq_configure_gradient.h"
 
-TEST(TESTS, Test_1) { ASSERT_EQ(1, 1); }
-TEST(TESTS, Test_2) { ASSERT_EQ(1, 1); }
-TEST(TESTS, Test_3) { ASSERT_EQ(1, 1); }
-TEST(TESTS, Test_4) { ASSERT_EQ(1, 1); }
-TEST(TESTS, Test_5) { ASSERT_EQ(1, 1); }
+TEST(TESTS, Test_size_1) {
+  int rank = 0;
+  MPI_Comm comm = MPI_COMM_WORLD;
+  MPI_Comm_rank(comm, &rank);
+
+  std::vector<double> A{10};
+  std::vector<double> b{12};
+
+  auto parResult = parConfigureGradientMethod(b.size(), A, b, comm);
+
+  if (rank == 0) {
+    auto seqResult = seqConfigureGradientMethod(b.size(), A, b);
+    ASSERT_TRUE(isEqualVectors(seqResult, parResult));
+  }
+}
+
+TEST(TESTS, Test_size_2) {
+  int rank = 0;
+  MPI_Comm comm = MPI_COMM_WORLD;
+  MPI_Comm_rank(comm, &rank);
+
+  std::vector<double> A{10, 1, 2, 3};
+  std::vector<double> b{12, 3};
+
+  auto parResult = parConfigureGradientMethod(b.size(), A, b, comm);
+
+  if (rank == 0) {
+    auto seqResult = seqConfigureGradientMethod(b.size(), A, b);
+    ASSERT_TRUE(isEqualVectors(seqResult, parResult));
+  }
+}
+
+TEST(TESTS, Test_size_3) {
+  int rank = 0;
+  MPI_Comm comm = MPI_COMM_WORLD;
+  MPI_Comm_rank(comm, &rank);
+
+  std::vector<double> A{10, 1, 2, 3, 1, 10, 2, 3, 1};
+  std::vector<double> b{12, 3, 11};
+
+  auto parResult = parConfigureGradientMethod(b.size(), A, b, comm);
+
+  if (rank == 0) {
+    auto seqResult = seqConfigureGradientMethod(b.size(), A, b);
+    ASSERT_TRUE(isEqualVectors(seqResult, parResult));
+  }
+}
+
+TEST(TESTS, Test_size_4) {
+  int rank = 0;
+  MPI_Comm comm = MPI_COMM_WORLD;
+  MPI_Comm_rank(comm, &rank);
+
+  std::vector<double> A{10, 1, 2, 3, 1, 10, 2, 3, 1, 0, 10, 2, 2, 0, 0, 10};
+  std::vector<double> b{12, 3, 11, 2};
+
+  auto parResult = parConfigureGradientMethod(b.size(), A, b, comm);
+
+  if (rank == 0) {
+    auto seqResult = seqConfigureGradientMethod(b.size(), A, b);
+    ASSERT_TRUE(isEqualVectors(seqResult, parResult));
+  }
+}
+
+TEST(TESTS, Test_empty) {
+  int rank = 0;
+  MPI_Comm comm = MPI_COMM_WORLD;
+  MPI_Comm_rank(comm, &rank);
+
+  std::vector<double> A{};
+  std::vector<double> b{};
+
+  ASSERT_ANY_THROW(parConfigureGradientMethod(b.size(), A, b, comm));
+}
 
 int main(int argc, char** argv) {
   int result_code = 0;
