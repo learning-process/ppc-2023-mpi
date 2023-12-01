648bb12b6220f2f7ed27ddaa40adf73fe5e56b58
Savotina <lerasavotina@gmail.com>
2023-12-01 22:38:12 +0300

    release

--- tasks/task_1/savotina_v_count_sym_in_str/main.cpp
+++ tasks/task_1/savotina_v_count_sym_in_str/main.cpp
@@ -3,11 +3,87 @@
 
 #include "./count_sym_in_str.h"
 
-TEST(TESTS, Test_1) { ASSERT_EQ(1, 1); }
-TEST(TESTS, Test_2) { ASSERT_EQ(1, 1); }
-TEST(TESTS, Test_3) { ASSERT_EQ(1, 1); }
-TEST(TESTS, Test_4) { ASSERT_EQ(1, 1); }
-TEST(TESTS, Test_5) { ASSERT_EQ(1, 1); }
+TEST(TESTS, Test_small_string) {
+  MPI_Comm comm = MPI_COMM_WORLD;
+  int rank = 0;
+  MPI_Comm_rank(comm, &rank);
+
+  std::string str{"Hello World!"};
+  char sym = 'l';
+
+  int parResult = parCountSymInString(str, sym, comm);
+
+  if (rank == 0) {
+    int seqResult = seqCountSymInString(str, sym);
+    ASSERT_EQ(seqResult, parResult);
+  }
+}
+
+TEST(TESTS, Test_count_is_zero) {
+  MPI_Comm comm = MPI_COMM_WORLD;
+  int rank = 0;
+  MPI_Comm_rank(comm, &rank);
+
+  std::string str{"010010010"};
+  char sym = '2';
+
+  int parResult = parCountSymInString(str, sym, comm);
+
+  if (rank == 0) {
+    int seqResult = seqCountSymInString(str, sym);
+    ASSERT_EQ(seqResult, parResult);
+  }
+}
+
+TEST(TESTS, Test_string_is_empty) {
+  MPI_Comm comm = MPI_COMM_WORLD;
+  int rank = 0;
+  MPI_Comm_rank(comm, &rank);
+
+  std::string str{};
+  char sym = '4';
+
+  int parResult = parCountSymInString(str, sym, comm);
+
+  if (rank == 0) {
+    int seqResult = seqCountSymInString(str, sym);
+    ASSERT_EQ(seqResult, parResult);
+  }
+}
+
+TEST(TESTS, Test_random_small) {
+  MPI_Comm comm = MPI_COMM_WORLD;
+  int rank = 0;
+  MPI_Comm_rank(comm, &rank);
+
+  size_t size = 10000;
+  std::string str{createRandomString(size)};
+  char sym = num_lim<char>::min() / 2;
+
+  int parResult = parCountSymInString(str, sym, comm);
+
+  if (rank == 0) {
+    int seqResult = seqCountSymInString(str, sym);
+    ASSERT_EQ(seqResult, parResult);
+  }
+}
+
+TEST(TESTS, Test_random_large) {
+  MPI_Comm comm = MPI_COMM_WORLD;
+  int rank = 0;
+  MPI_Comm_rank(comm, &rank);
+
+  size_t size = 1'000'000;
+  std::string str{createRandomString(size)};
+  char sym = num_lim<char>::max() / 2;
+
+  int parResult = parCountSymInString(str, sym, comm);
+
+  if (rank == 0) {
+    int seqResult = seqCountSymInString(str, sym);
+    ASSERT_EQ(seqResult, parResult);
+  }
+}
 
 int main(int argc, char** argv) {
   int result_code = 0;
