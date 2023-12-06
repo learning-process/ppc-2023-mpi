7c60ffd89859179c89095a2bcda834cd1851877a
Kirill23323 <KirillProkofev12345@yandex.ru>
2023-12-06 21:55:47 +0300

    another try

--- tasks/task_3/prokofev_k_Shell_sort_with_Batcher_merge/sort_realization.h
+++ tasks/task_3/prokofev_k_Shell_sort_with_Batcher_merge/sort_realization.h
@@ -1,6 +1,6 @@
 // Copyright 2023 Prokofev Kirill
-#ifndef TASKS_TASK_3_PROKOFEV_K_SHELL_SORT_WITH_BATCHER_MERGE_H_
-#define TASKS_TASK_3_PROKOFEV_K_SHELL_SORT_WITH_BATCHER_MERGE_H_
+#ifndef TASKS_TASK_3_PROKOFEV_K_SHELL_SORT_WITH_BATCHER_MERGE_SORT_REALIZATION_H_
+#define TASKS_TASK_3_PROKOFEV_K_SHELL_SORT_WITH_BATCHER_MERGE_SORT_REALIZATION_H_
 
 #include <mpi.h>
 #include <cstring>
@@ -8,10 +8,11 @@
 #include <vector>
 #include <random>
 
-void ShellSortSeq(std::vector<int>& vec);
-std::vector<int> BatcherMerge(const std::vector<int>& vec_1,
-  const std::vector<int>& vec_2);
-void ShellSortParallel(std::vector<int>& vec);
+void ShellSortSeq(std::vector<int>* vec);
+void ShellSortParallel(std::vector<int>* vec);
+void BatcherMerge(std::vector<int>* vec, int l, int m, int r);
+void BatcherOddEvenMerge(std::vector<int>* vec, int start, int end, int dist);
+void BatcherSort(std::vector<int>* arr);
 std::vector<int> GenerateRandomVector(int n);
 
-#endif  // TASKS_TASK_3_PROKOFEV_K_SHELL_SORT_WITH_BATCHER_MERGE_H_
+#endif  // TASKS_TASK_3_PROKOFEV_K_SHELL_SORT_WITH_BATCHER_MERGE_SORT_REALIZATION_H_
