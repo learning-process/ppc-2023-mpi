0f6ac3ad99d8b91eab4176e26312c4b7b04ce3fe
Kirill23323 <KirillProkofev12345@yandex.ru>
2023-12-06 21:55:23 +0300

    another try

--- tasks/task_3/prokofev_k_Shell_sort_with_Batcher_merge/sort_realization.cpp
+++ tasks/task_3/prokofev_k_Shell_sort_with_Batcher_merge/sort_realization.cpp
@@ -1,102 +1,127 @@
 // Copyright 2023 Prokofev Kirill
 #include "task_3/prokofev_k_Shell_sort_with_Batcher_merge/sort_realization.h"
 
-void ShellSortSeq(std::vector<int>& vec) {
-  int vecSize = vec.size();
-  int k,j,i;
-  for (k = vecSize / 2; k > 0; k /= 2) {
-    for (i = k; i < vecSize; i++) {
-      int t = vec[i];
-      for (j = i; j >= k; j -= k) {
-        if (t < vec[j - k]) {
-          vec[j] = vec[j - k];
-        } else {
-          break;
-        }
+void ShellSortSeq(std::vector<int>* vec) {
+  int vecSize = vec->size();
+  int gap = 1;
+  while (gap < vecSize / 3) {
+    gap = 3 * gap + 1;
+  }
+  while (gap > 0) {
+    for (int i = gap; i < vecSize; ++i) {
+      int temp = (*vec)[i];
+      int j = i;
+      while (j >= gap && (*vec)[j - gap] > temp) {
+        (*vec)[j] = (*vec)[j - gap];
+        j -= gap;
       }
-      vec[j] = t;
+      (*vec)[j] = temp;
     }
+    gap /= 3;
   }
-}
-std::vector<int> BatcherMerge(const std::vector<int>& vec_1, const std::vector<int>& vec_2)
-{
-  std::vector<int> resultVector(vec_1.size() + vec_2.size());
-  size_t i = 0, j = 0, k = 0;
-  while (i < vec_1.size() && j < vec_2.size()) {
-    resultVector[k++] = (vec_1[i] < vec_2[j] ? vec_1[i++] : vec_2[j++]);
-  }
-  while (i < vec_1.size()) {
-    resultVector[k++] = vec_1[i++];
-  }
-  while (j < vec_2.size()) {
-    resultVector[k++] = vec_2[j++];
-  }
-  return resultVector;
 }
 
-void ShellSortParallel(std::vector<int>& vec) {
+void ShellSortParallel(std::vector<int>* vec) {
   int numProc;
   MPI_Comm_size(MPI_COMM_WORLD, &numProc);
   int rankProc;
   MPI_Comm_rank(MPI_COMM_WORLD, &rankProc);
-  int mainVecSize = vec.size();
-  std::vector<int> preResultVector(vec.size());
-  int remainder = vec.size() % numProc;
-  int numElemInLocalVector = vec.size() / numProc;
+  int remainder = vec->size() % numProc;
+  std::vector<int> preResultVector(vec->size() - remainder);
+  int numElemInLocalVector = vec->size() / numProc;
   std::vector<int>localVector(numElemInLocalVector);
-  MPI_Scatter(vec.data(), numElemInLocalVector, MPI_INT, localVector.data(),
+  MPI_Scatter(vec->data(), numElemInLocalVector, MPI_INT, localVector.data(),
     numElemInLocalVector, MPI_INT, 0, MPI_COMM_WORLD);
-  ShellSortSeq(localVector);
+  ShellSortSeq(&localVector);
   MPI_Gather(localVector.data(), numElemInLocalVector, MPI_INT,
     preResultVector.data(), numElemInLocalVector, MPI_INT, 0, MPI_COMM_WORLD);
   if (rankProc == 0) {
-    std::vector<std::vector<int>> preResultVectorContainer;
     if (remainder > 0) {
       std::vector<int> remainderVect;
       int k = remainder;
-      int remIndex = vec.size() - 1;
+      int remIndex = vec->size() - 1;
       while (k != 0) {
-        remainderVect.push_back(vec[remIndex]);
+        remainderVect.push_back((*vec)[remIndex]);
         remIndex--;
         k--;
       }
-      while (remainder != numElemInLocalVector) 
-      {
-        remainderVect.push_back(INT_MIN);
-        remainder++;
+      ShellSortSeq(&remainderVect);
+      for (int i = 0; i < remainderVect.size(); i++) {
+        preResultVector.push_back(remainderVect[i]);
       }
-      ShellSortSeq(remainderVect);
-      preResultVectorContainer.push_back(remainderVect);
     }
-    for (int i = 0; i < numProc; i++) 
-    {
-      std::vector<int> tmpVect;
-      int k = numElemInLocalVector;
-      int index = i * numElemInLocalVector;
-      while (k != 0) {
-        tmpVect.push_back(preResultVector[index]);
-        index++;
-        k--;
-      }
-      preResultVectorContainer.push_back(tmpVect);
-    }
-    vec = preResultVectorContainer[0];
-    for (int i = 1; i < preResultVectorContainer.size(); i++)
-      vec = BatcherMerge(vec, preResultVectorContainer[i]);
-    if (vec.size() != mainVecSize)
-      vec.erase(std::remove(vec.begin(),
-        vec.end(), INT_MIN), vec.end());
+    BatcherSort(&preResultVector);
+    *vec = preResultVector;
   }
- }
+}
 
-std::vector<int> GenerateRandomVector(int n)
-{
+void BatcherMerge(std::vector<int>* vec, int l, int m, int r) {
+  int n1 = m - l + 1;
+  int n2 = r - m;
+  std::vector<int> L(n1);
+  std::vector<int> R(n2);
+  for (int i = 0; i < n1; ++i)
+    L[i] = (*vec)[l + i];
+  for (int j = 0; j < n2; ++j)
+    R[j] = (*vec)[m + 1 + j];
+  int i = 0, j = 0, k = l;
+  while (i < n1 && j < n2) {
+    if (L[i] <= R[j]) {
+      (*vec)[k] = L[i];
+      ++i;
+    }
+    else {
+      (*vec)[k] = R[j];
+      ++j;
+    }
+    k++;
+  }
+  while (i < n1) {
+    (*vec)[k] = L[i];
+    ++i;
+    ++k;
+  }
+}
+
+void BatcherOddEvenMerge(std::vector<int>* vec, int start, int end, int dist) {
+  if (dist > 1) {
+    int mid = dist / 2;
+    BatcherOddEvenMerge(vec, start, start + mid, mid);
+    BatcherOddEvenMerge(vec, start + mid, end, mid);
+    for (int i = start; i + mid < end; ++i) {
+      if (i % dist < mid) {
+        int l = i;
+        int r = i + mid;
+        int mergeEnd = std::min(r + mid, end);
+        BatcherMerge(vec, l, r - 1, mergeEnd - 1);
+      }
+    }
+  }
+}
+
+void BatcherSort(std::vector<int>* vec) {
+  int n = vec->size();
+  int powerOfTwo = 1;
+  while (powerOfTwo < n) {
+    powerOfTwo <<= 1;
+  }
+  if (powerOfTwo != n) {
+    for (int i = 0; i < powerOfTwo - n; i++)
+      vec->push_back(INT_MIN);
+    BatcherOddEvenMerge(vec, 0, powerOfTwo, powerOfTwo);
+    vec->erase(std::remove(vec->begin(), vec->end(), INT_MIN), vec->end());
+    return;
+  }
+  BatcherOddEvenMerge(vec, 0, powerOfTwo, powerOfTwo);
+}
+
+std::vector<int> GenerateRandomVector(int n) {
   if (n <= 0)
     throw "Error";
   std::random_device dev;
   std::mt19937 generate(dev());
   std::vector<int> newVector(n);
-  for (int i = 0; i < n; i++){
+  for (int i = 0; i < n; i++) {
     newVector[i] = generate() % n;
   }
   return newVector;
