5835f2c7d04780e4830ba4783417225cff825815
Egor Kosarev <kosegor2003@yandex.ru>
2023-12-10 16:23:42 +0300

    fix style5

--- tasks/task_3/kosarev_e_quick_sort_merge/quick_sort_merge.h
+++ tasks/task_3/kosarev_e_quick_sort_merge/quick_sort_merge.h
@@ -2,17 +2,18 @@
 
 #ifndef TASKS_TASK_3_KOSAREV_E_QUICK_SORT_MERGE_QUICK_SORT_MERGE_H_
 #define TASKS_TASK_3_KOSAREV_E_QUICK_SORT_MERGE_QUICK_SORT_MERGE_H_
+
+#include <mpi.h>
 #include <random>
 #include <vector>
 #include <utility>
 #include <iostream>
 #include <algorithm>
-#include <header.h>
-#include <mpi.h>
 
 std::pair<int, int> split(int* array, int n);
 void recursive_quick_sort(int* array, int n);
 void Merge(int* array1, int size1, int size2);
 void parallelQuickSort(int* array, size_t n);
 bool checking(int* array, int n);
+
 #endif  // TASKS_TASK_3_KOSAREV_E_QUICK_SORT_MERGE_QUICK_SORT_MERGE_H_
