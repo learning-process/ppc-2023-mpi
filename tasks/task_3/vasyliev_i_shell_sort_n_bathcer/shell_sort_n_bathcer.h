7af9b1065f12675921dce940339d8da3a9e48850
AdAndRoll <92474605+AdAndRoll@users.noreply.github.com>
2023-12-16 02:08:33 +0300

    add task3

---
+++ tasks/task_3/vasyliev_i_shell_sort_n_bathcer/shell_sort_n_bathcer.h
@@ -0,0 +1,9 @@
+// Copyright 2023 Vasyliev Ivan
+#ifndef TASKS_TASK_3_VASYLIEV_I_SHELL_SORT_N_BATCHER_SHELL_SORT_N_BATCHER_H_
+#define TASKS_TASK_3_VASYLIEV_I_SHELL_SORT_N_BATCHER_SHELL_SORT_N_BATCHER_H_
+#include <vector>
+
+std::vector<int> getRandomVector(int n, int lb, int ub);
+std::vector<int> ParallelSort(std::vector<int> vec, int vec_sz);
+
+#endif  // TASKS_TASK_3_VASYLIEV_I_SHELL_SORT_N_BATCHER_SHELL_SORT_N_BATCHER_H_
