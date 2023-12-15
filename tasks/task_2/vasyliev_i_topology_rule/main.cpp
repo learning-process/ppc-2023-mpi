c6105c34c9a785b9806c900bed5af362e7288a80
AdAndRoll <92474605+AdAndRoll@users.noreply.github.com>
2023-12-16 00:50:41 +0300

    add tests

--- tasks/task_2/vasyliev_i_topology_rule/main.cpp
+++ tasks/task_2/vasyliev_i_topology_rule/main.cpp
@@ -8,15 +8,84 @@
 TEST(TopologyRuleTest, Test1) {
     boost::mpi::communicator world;
     if (world.size() == 1) { return; }
+    int src = 0, dst = world.size() - 1;
     int data = 0, tag = 0;
-    if (world.rank() == 0) {
+    if (world.rank() == src) {
         data = 42;
-        RULE_SEND(world, data, world.size() - 1, tag);
-    } else if (world.rank() == world.size() - 1) {
-        RULE_RECV(world, data, 0, tag);
+        RULE_SEND(world, data, dst, tag);
+    } else if (world.rank() == dst) {
+        RULE_RECV(world, data, src, tag);
         ASSERT_EQ(data, 42);
     } else {
-        RULE_HELP(world, 0, world.size() - 1, tag);
+        RULE_HELP(world, src,  dst, tag);
+    }
+}
+
+
+TEST(TopologyRuleTest, Test2) {
+    boost::mpi::communicator world;
+    if (world.size() == 1) { return; }
+    int src = world.size() - 1, dst = 0;
+    int data = 0, tag = 0;
+    if (world.rank() == src) {
+        data = 42;
+        RULE_SEND(world, data, dst, tag);
+    } else if (world.rank() == dst) {
+        RULE_RECV(world, data, src, tag);
+        ASSERT_EQ(data, 42);
+    } else {
+        RULE_HELP(world, src,  dst, tag);
+    }
+}
+
+
+TEST(TopologyRuleTest, Test3) {
+    boost::mpi::communicator world;
+    int src = 0, dst = world.size() / 2;
+    if (src == dst) { return; }
+    int data = 0, tag = 0;
+    if (world.rank() == src) {
+        data = 42;
+        RULE_SEND(world, data, dst, tag);
+    } else if (world.rank() == dst) {
+        RULE_RECV(world, data, src, tag);
+        ASSERT_EQ(data, 42);
+    } else {
+        RULE_HELP(world, src, dst, tag);
+    }
+}
+
+
+TEST(TopologyRuleTest, Test4) {
+    boost::mpi::communicator world;
+    int src =  world.size() / 2, dst = 0;
+    if (src == dst) { return; }
+    int data = 0, tag = 0;
+    if (world.rank() == src) {
+        data = 42;
+        RULE_SEND(world, data, dst, tag);
+    } else if (world.rank() == dst) {
+        RULE_RECV(world, data, src, tag);
+        ASSERT_EQ(data, 42);
+    } else {
+        RULE_HELP(world, src, dst, tag);
+    }
+}
+
+
+TEST(TopologyRuleTest, Test5) {
+    boost::mpi::communicator world;
+    if (world.size() == 1) { return; }
+    int src = 0, dst = world.size() - 1;
+    int data = 0, tag = 8080;
+    if (world.rank() == src) {
+        data = 42;
+        RULE_SEND(world, data, dst, tag);
+    } else if (world.rank() == dst) {
+        RULE_RECV(world, data, src, tag);
+        ASSERT_EQ(data, 42);
+    } else {
+        RULE_HELP(world, src, dst, tag);
     }
 }
 
