ebd6089ff5e8df5e3baa272c5a8d3102d049c639
Tatiana567 <tatiana.tutcheva145@gmail.com>
2023-12-18 20:52:05 +0300

    may2

--- tasks/task_3/benduyzhko_t_moors_algoritm/moors_algoritm.cpp
+++ tasks/task_3/benduyzhko_t_moors_algoritm/moors_algoritm.cpp
@@ -9,7 +9,7 @@
 #include <stdexcept>
 #include <algorithm>
 #include <limits>
-#include "task_3/benduyzko_a_moors_algoritm/moors_algoritm.h"
+#include "tasks/task_3/benduyzhko_t_moors_algoritm/moors_algoritm.h"
 
 static int offset = 0;
 const int INF = 2000000000;
