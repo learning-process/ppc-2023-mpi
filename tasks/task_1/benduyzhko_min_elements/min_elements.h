af18068af5158a77be74a0c9e1963d03509d96f7
Tatiana567 <tatiana.tutcheva145@gmail.com>
2023-12-18 20:47:36 +0300

    mrrr2

--- tasks/task_1/benduyzhko_min_elements/min_elements.h
+++ tasks/task_1/benduyzhko_min_elements/min_elements.h
@@ -1,6 +1,6 @@
 // Copyright 2023 Benduyzhko Tatiana
-#ifndef TASKS_EXAMPLES_TEST_MPI_OPS_MPI_H_
-#define TASKS_EXAMPLES_TEST_MPI_OPS_MPI_H_
+#ifndef TASKS_TASK_1_BENDUYZHKO_MIN_ELEMENTS_MIN_ELEMENTS_H_
+#define TASKS_TASK_1_BENDUYZHKO_MIN_ELEMENTS_MIN_ELEMENTS_H_
 
 #include <vector>
 #include <string>
@@ -10,4 +10,4 @@ int getParallelOperations(std::vector<int> global_vec,
                           int count_size_vector, const std::string& ops);
 int getSequentialOperations(std::vector<int> vec, const std::string& ops);
 
-#endif  // TASKS_EXAMPLES_TEST_MPI_OPS_MPI_H_
+#endif  // TASKS_TASK_1_BENDUYZHKO_MIN_ELEMENTS_MIN_ELEMENTS_H_
