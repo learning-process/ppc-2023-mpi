f55101f74c16ea07828ef437120898296077bb8f
Trolozor <90347431+Trolozor@users.noreply.github.com>
2023-12-20 00:20:53 +0300

    fix

---
+++ tasks/task_1/dostavalov_s_monte_carlo_method/monte_carlo_method.h
@@ -0,0 +1,10 @@
+﻿// Copyright 2023 Dostavalov Semen
+#pragma once
+#include <iostream>
+
+#ifndef MODULES_TASK_1_DOSTAVALOV_S_MONTE_CARLO_METHOD_H_
+#define MODULES_TASK_1_DOSTAVALOV_S_MONTE_CARLO_METHOD_H_
+
+double monteCarlo(int low, int high, int count, double (*f)(double));
+
+#endif // MODULES_TASK_1_DOSTAVALOV_S_MONTE_CARLO_METHOD_H_
