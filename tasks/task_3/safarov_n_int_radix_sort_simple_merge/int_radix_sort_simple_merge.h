b0ffbe8b57d40049cb8ee30b5230c9a5c67da5d7
Nurlan Safarov <safarov_nm@mail.ru>
2023-12-20 21:26:05 +0300

    3 laboratory work on PPC (Fixed | Google by Style)

--- tasks/task_3/safarov_n_int_radix_sort_simple_merge/int_radix_sort_simple_merge.h
+++ tasks/task_3/safarov_n_int_radix_sort_simple_merge/int_radix_sort_simple_merge.h
@@ -1,6 +1,6 @@
 // Copyright 2023 Safarov Nurlan
-#ifndef TASKS_TASK_3_SAFAROV_N_INT_RADIX_SORT_SIMPLE_MERGE_H_
-#define TASKS_TASK_3_SAFAROV_N_INT_RADIX_SORT_SIMPLE_MERGE_H_
+#ifndef TASKS_TASK_3_SAFAROV_N_INT_RADIX_SORT_SIMPLE_MERGE_INT_RADIX_SORT_SIMPLE_MERGE_H_
+#define TASKS_TASK_3_SAFAROV_N_INT_RADIX_SORT_SIMPLE_MERGE_INT_RADIX_SORT_SIMPLE_MERGE_H_
 
 #include <mpi.h>
 #include <vector>
@@ -14,4 +14,4 @@ std::vector<int> mergeSortedVectors(std::vector<std::vector<int>> m);
 std::vector<int> radixSortWithSimpleMergeSequential(std::vector<int> vector);
 std::vector<int> radixSortWithSimpleMergeParallel(std::vector<int> vector, int sizeOfVector);
 
-#endif  // TASKS_TASK_3_SAFAROV_N_INT_RADIX_SORT_SIMPLE_MERGE_H_
+#endif  // TASKS_TASK_3_SAFAROV_N_INT_RADIX_SORT_SIMPLE_MERGE_INT_RADIX_SORT_SIMPLE_MERGE_H_
