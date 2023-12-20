ad910a80993675b6e9f34b7d2fed45b6984f18a0
Trolozor <90347431+Trolozor@users.noreply.github.com>
2023-12-21 01:50:07 +0300

    Add files via upload

---
+++ tasks/task_1/dostavalov_s_monte_carlo_method/monte_carlo.h
@@ -0,0 +1,7 @@
+// Copyright 2023 Dostavalov Semen
+#ifndef TASKS_TASK_1_DOSTAVALOV_S_MONTE_CARLO_METHOD_MONTE_CARLO_H_
+#define TASKS_TASK_1_DOSTAVALOV_S_MONTE_CARLO_METHOD_MONTE_CARLO_H_
+
+double monteCarlo(int low, int high, int count, double (*f)(double));
+
+#endif  // TASKS_TASK_1_DOSTAVALOV_S_MONTE_CARLO_METHOD_MONTE_CARLO_H_
