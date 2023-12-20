178851dee16900eac124182cba4e6a4424eca61d
krllvv <krllv@pm.me>
2023-12-20 22:40:27 +0300

    Updated: matrix_column_max.cpp, added: main.cpp

--- tasks/task_1/kirillov_m_max_by_columns_matrix/matrix_column_max.cpp
+++ tasks/task_1/kirillov_m_max_by_columns_matrix/matrix_column_max.cpp
@@ -5,7 +5,7 @@
 #include <boost/mpi/communicator.hpp>
 #include <boost/mpi/collectives.hpp>
 
-std::pair<int, int> getColIndexAndCount(size_t columns);
+std::pair<int, int> getColIndexAndCount(size_t columns, int rank);
 std::vector<int> getRandomMatrix(int rows, int columns){
     std::random_device dev;
     std::mt19937 gen(dev());
@@ -32,8 +32,9 @@ std::vector<int> getParallelMaxInColumns(std::vector<int>&matrix, size_t rows, s
     if (world.rank() != 0){
         matrix.resize(rows*columns);
     }
+
     boost::mpi::broadcast(world, matrix.data(), rows * columns, 0);
-    auto [colsCount, colIndex] = getColIndexAndCount(columns);
+    auto [colsCount, colIndex] = getColIndexAndCount(columns, world.rank());
     std::vector<int> localMaxValues(colsCount);
     for (int i = colIndex; i < colsCount + colIndex; i++){
         for (int j = 0; j < rows; j++) {
@@ -41,25 +42,31 @@ std::vector<int> getParallelMaxInColumns(std::vector<int>&matrix, size_t rows, s
         }
     }
     if (world.rank() != 0){
-        boost::mpi::scatter(world, localMaxValues.data(), colsCount, 0);
+        boost::mpi::gatherv(world, localMaxValues, 0);
     }
     else {
         std::vector<int> globalMaxValues(columns);
-        boost::mpi::scatter(world, globalMaxValues, localMaxValues.data(), colsCount, 0);
+        std::vector<int> counts(world.size()), offsets(world.size());
+        for(int i = 0; i < world.size(); i++){
+            auto [count, offset] = getColIndexAndCount(columns, i);
+            counts[i] = count;
+            offsets[i] = offset;
+        }
+        boost::mpi::gatherv(world, localMaxValues, globalMaxValues.data(), counts, offsets, 0);
         return globalMaxValues;
     }
     return {};
 }
 
-std::pair<int, int> getColIndexAndCount(size_t columns){
+std::pair<int, int> getColIndexAndCount(size_t columns, int rank){
     boost::mpi::communicator world;
     int size = world.size();
-    int rank = world.rank();
     int colsPerProcess = columns / size;
     int reminder = columns % size;
-    // 6 / 3 = 2 (colsperpr)
-    // 8 / 3 = 2, 8 % 3 = 2,
     int colsCount = colsPerProcess + (rank < reminder ? 1 : 0);
     int colIndex = colsPerProcess * rank + (rank < reminder ? rank : reminder);
+    if (rank >= columns) {
+        colIndex = 0;
+    }
     return std::make_pair(colsCount, colIndex);
 }
