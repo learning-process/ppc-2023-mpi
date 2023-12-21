cadf48d3c8ba7ca7c0b53718762759906f0de086
Ivan Vinokurov <39878097+LeakMachine@users.noreply.github.com>
2023-12-21 21:26:57 +0300

    code fix vol 2

--- tasks/task_2/vinokurov_i_seidel_iteration_method/seidel_iteration_method.cpp
+++
@@ -1,9 +0,0 @@
-// Copyright 2023 Vinokurov Ivan
-#include <random>
-#include <algorithm>
-#include <functional>
-#include <vector>
-
-#include "task_2/vinokurov_i_seidel_iteration_method/seidel_iteration_method.h"
-
-
