18f4f560d6000d4be1e1eeb103007ff3852f314d
SmirnovLM <smirnoff.2003@yandex.ru>
2023-12-21 19:56:49 +0300

    s17

--- tasks/task_3/smirnov_l_search_area/main.cpp
+++ tasks/task_3/smirnov_l_search_area/main.cpp
@@ -167,6 +167,7 @@ int main(int argc, char** argv) {
     ::testing::InitGoogleTest(&argc, argv);
     ::testing::TestEventListeners& listeners =
         ::testing::UnitTest::GetInstance()->listeners();
+
     int rank;
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
     if (rank != 0) {
