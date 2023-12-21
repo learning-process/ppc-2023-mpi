d1d0f243ff51eeff968f93b0017c0988dbd5841e
SmirnovLM <smirnoff.2003@yandex.ru>
2023-12-21 19:59:40 +0300

    s18

--- tasks/task_3/smirnov_l_search_area/main.cpp
+++ tasks/task_3/smirnov_l_search_area/main.cpp
@@ -170,6 +170,7 @@ int main(int argc, char** argv) {
 
     int rank;
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+
     if (rank != 0) {
         delete listeners.Release(listeners.default_result_printer());
     }
