c5989bc86f69d493c7cb12ad0dfe16e327819ec3
Sasha <sascha.kalinin@mail.ru>
2023-12-22 00:23:09 +0300

    Task done

---
+++ tasks/task_2/kalinin_a_gorA_verB/gorA_verB.h
@@ -0,0 +1,18 @@
+// Copyright 2023 Kalinin Alexandr
+#ifndef TASKS_TASK_1_KALININ_A_LEX_STRINGS_LEX_STRINGS_H_
+#define TASKS_TASK_1_KALININ_A_LEX_STRINGS_LEX_STRINGS_H_
+#include <mpi.h>
+
+#include <vector>
+#include <algorithm>
+#include <random>
+
+std::vector<int> getRandomMatrix(int row_count, int column_count);
+std::vector<int> getParallelOperations(const std::vector<int>& matrix1,
+    const std::vector<int>& matrix2, int row_count_matrix1,
+    int column_count_matrix1);
+std::vector<int> getSequentialOperations(const std::vector<int>& matrix1,
+    const std::vector<int>& matrix2, int row_count_matrix1,
+    int column_count_matrix1, int column_count_matrix2);
+
+#endif  // TASKS_TASK_1_KALININ_A_LEX_STRINGS_LEX_STRINGS_H_
