1dc0fbd678e6a683163cdfc16f4a99cac69de9e8
SmirnovLM <smirnoff.2003@yandex.ru>
2023-12-21 17:43:46 +0300

    s10

--- tasks/task_3/smirnov_l_search_area/search_area.cpp
+++ tasks/task_3/smirnov_l_search_area/search_area.cpp
@@ -17,7 +17,7 @@ double ApplyGlobalOptimizationSequential(
     std::vector<double> interimResults;
     interimResults.push_back(lowerBound);
     interimResults.push_back(upperBound);
-
+    // 8
     int maxContr = 1;
     double coef = 2, controlParameter;
 
@@ -45,7 +45,7 @@ double ApplyGlobalOptimizationSequential(
             function(&interimResults.at(1)) -
             function(&interimResults.at(0)))
             / (interimResults.at(1) - interimResults.at(0));
-        //olo
+        
         for (int i = 2; i <= counter; i++) {
             meanRateOfChange = std::max(
                 meanRateOfChange,
