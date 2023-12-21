a940ee6bca34a95b2da98fb05c9685b3cbd29bb8
SmirnovLM <smirnoff.2003@yandex.ru>
2023-12-21 11:59:50 +0300

    s1

---
+++ tasks/task_3/smirnov_l_search_area/search_area.h
@@ -0,0 +1,24 @@
+// Copyright 2023 Smirnov Leonid
+#pragma once
+
+#include <mpi.h>
+#include <cmath>
+#include <vector>
+#include <algorithm>
+#include <functional>
+
+double ApplyGlobalOptimizationSequential(
+    std::function<double(double*)> function,
+    const double lowerBound, 
+    const double upperBound,
+    const int numberPartitions,
+    const double accuracy
+    );
+
+double ApplyGlobalOptimizationParrallel(
+    std::function<double(double*)> function,
+    const double lowerBound,
+    const double upperBound,
+    const int numberPartitions,
+    const double accuracy
+);
