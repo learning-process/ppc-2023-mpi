3d301bb2499a444c83e86dfb28daa6f0de71194e
Sasha <alexkalinin19756@mail.ru>
2023-12-22 21:18:45 +0300

    Fix style

--- tasks/task_3/kalinin_a_gradient/gradient.h
+++ tasks/task_3/kalinin_a_gradient/gradient.h
@@ -16,9 +16,9 @@ double scalar_mult(const Vector& x, const Vector& y);
 Vector mult_MxV(const Vector& M, const Vector& V);
 
 Vector Serial_method_gradient(const Vector& M,
-	const Vector& V, int n);
+    const Vector& V, int n);
 
 Vector Paralle_method_gradient(const Vector& M,
-	const Vector& V, int n);
+    const Vector& V, int n);
 
 #endif  // TASKS_TASK_3_KALININ_A_GRADIENT_GRADIENT_H_
