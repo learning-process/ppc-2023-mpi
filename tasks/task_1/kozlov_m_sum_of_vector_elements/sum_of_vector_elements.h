ce2b22489a32297a7b9ec0f5e6addbf953f1fbb5
mixa <mihsa11ru@gmail.com>
2023-12-22 20:13:15 +0300

    1st

---
+++ tasks/task_1/kozlov_m_sum_of_vector_elements/sum_of_vector_elements.h
@@ -0,0 +1,12 @@
+
+#ifndef TASKS_TASK_1_KOZLOV_M_SUM_OF_VECTOR_ELEMENTS_SUM_OF_VECTOR_ELEMENTS_H_
+#define TASKS_TASK_1_KOZLOV_M_SUM_OF_VECTOR_ELEMENTS_SUM_OF_VECTOR_ELEMENTS_H_
+
+#include <mpi.h>
+#include <vector>
+
+int sum_of_vec_elem_seq(const std::vector<int> &vec);
+int sum_of_vec_elem_par(const std::vector<int> &vec);
+std::vector<int> get_random_vector(int sz);
+
+#endif  // TASKS_TASK_1_KOZLOV_M_SUM_OF_VECTOR_ELEMENTS_SUM_OF_VECTOR_ELEMENTS_H_
