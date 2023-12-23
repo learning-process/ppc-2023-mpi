0253d16a84835db23acae68ebccd3a1f5746cf6f
Makhinya Danil <4mada0@gmail.com>
2023-12-23 21:58:26 +0300

    task3

--- tasks/task_3/makhinya_d_conjugate_gradient_method/gradient_method.h
+++ tasks/task_3/makhinya_d_conjugate_gradient_method/gradient_method.h
@@ -4,9 +4,16 @@
 
 #include <vector>
 
+#define MAX_COUNT_ITER 100000
+#define EPS 1e-4
+
 using matrix = std::vector<std::vector<double>>;
 using vector = std::vector<double>;
 
 vector solve_parallel(const matrix& A, const vector& b);
+double measure(const vector& r);
+vector operator* (double c, const vector& b);
+vector operator+ (const vector& a, const vector& b);
+vector operator- (const vector& a, const vector& b);
 
 #endif  // TASKS_TASK_2_MAKHINYA_D_GRADIENT_METHOD_GRADIENT_METHOD_H_
