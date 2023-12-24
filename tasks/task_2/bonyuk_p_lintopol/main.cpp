cacfbc5df410416372c39a16dd89902e3da18431
PetrBonyuk <152027126+PetrBonyuk@users.noreply.github.com>
2023-12-24 22:47:55 +0300

    gdfdfqwe

---
+++ tasks/task_2/bonyuk_p_lintopol/main.cpp
@@ -0,0 +1,66 @@
+﻿  // Copyright 2023 Bonyuk Petr
+
+#include <mpi.h>
+#include <gtest/gtest.h>
+#include "task_2/bonyuk_p_lintopol/lintopol.h"
+
+const int TEST_DATA_VALUE = 100;
+
+void initializeTestData(int rank, int sender, int& data) {
+    if (rank == sender) {
+        data = TEST_DATA_VALUE;
+    }
+}
+
+void runLinearTopologyTest(int sender, int receiver, MPI_Comm comm) {
+    int rank, size;
+    MPI_Comm_rank(comm, &rank);
+    MPI_Comm_size(comm, &size);
+
+    int data = 0;
+    initializeTestData(rank, sender, data);
+
+    Sends_data_lin_acr_the_topol(&data, 1, MPI_INT, sender, receiver, 0, comm);
+
+    if (rank == receiver) {
+        ASSERT_EQ(TEST_DATA_VALUE, data);
+    }
+}
+
+TEST(LinearTopol, TestMiddleSendDec) {
+    runLinearTopologyTest(2, 0, MPI_COMM_WORLD);
+}
+
+TEST(LinearTopol, TestFirstSend) {
+    runLinearTopologyTest(0, 3, MPI_COMM_WORLD);
+}
+
+TEST(LinearTopol, TestLastSend) {
+    runLinearTopologyTest(3, 0, MPI_COMM_WORLD);
+}
+
+TEST(LinearTopol, TestMultipleSends) {
+    runLinearTopologyTest(2, 3, MPI_COMM_WORLD);
+}
+
+TEST(LinearTopol, TestMultipleSends) {
+    runLinearTopologyTest(0, 2, MPI_COMM_WORLD);
+}
+
+int main(int argc, char** argv) {
+    int result_code = 0;
+
+    ::testing::InitGoogleTest(&argc, argv);
+    ::testing::TestEventListeners& listeners =
+        ::testing::UnitTest::GetInstance()->listeners();
+
+    if (MPI_Init(&argc, &argv) != MPI_SUCCESS) {
+       std::cerr << "MPI initialization failed" << std::endl;
+       MPI_Abort(MPI_COMM_WORLD, -1);
+    }
+
+    result_code = RUN_ALL_TESTS();
+    MPI_Finalize();
+
+    return result_code;
+}
