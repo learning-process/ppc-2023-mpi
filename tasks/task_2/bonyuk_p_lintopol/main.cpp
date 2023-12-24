6d42f9cdcf1d60765c5bea7cb26ba82251b793bc
PetrBonyuk <152027126+PetrBonyuk@users.noreply.github.com>
2023-12-24 23:13:52 +0300

    gddtgkgd

--- tasks/task_2/bonyuk_p_lintopol/main.cpp
+++ tasks/task_2/bonyuk_p_lintopol/main.cpp
@@ -6,61 +6,87 @@
 
 const int TEST_DATA_VALUE = 100;
 
-void initializeTestData(int rank, int sender, int &data) {
+int runLinearTopologyTest(int sender, int receiver, MPI_Comm comm) {
+    int rank;
+    MPI_Comm_rank(comm, &rank);
+
+    int data = 0;
     if (rank == sender) {
         data = TEST_DATA_VALUE;
     }
-}
-
-void runLinearTopologyTest(int sender, int receiver, MPI_Comm comm) {
-    int rank, size;
-    MPI_Comm_rank(comm, &rank);
-    MPI_Comm_size(comm, &size);
-
-    int data = 0;
-    initializeTestData(rank, sender, data);
 
     Sends_data_lin_acr_the_topol(&data, 1, MPI_INT, sender, receiver, 0, comm);
 
     if (rank == receiver) {
-        ASSERT_EQ(TEST_DATA_VALUE, data);
+		return data;
+    } else {
+       return -1;
     }
+}
+
+void testWithMultipleProcesses(std::function<int(MPI_Comm)> testFunc, int expectedValue, MPI_Comm comm) {
+    int world_rank;
+    MPI_Comm_rank(comm, &world_rank);
+
+    int value = testFunc(comm);
+
+    if (world_rank == 0) {
+        int result;
+        MPI_Reduce(&value, &result, 1, MPI_INT, MPI_MAX, 0, comm);
+        ASSERT_EQ(result, expectedValue);
+    } else if (value != -1) {
+        MPI_Reduce(&value, nullptr, 1, MPI_INT, MPI_MAX, 0, comm);
+    }
+    MPI_Barrier(comm);
 }
 
 TEST(LinearTopol, TestMiddleSendDec) {
-    runLinearTopologyTest(2, 0, MPI_COMM_WORLD);
+    testWithMultipleProcesses([](MPI_Comm comm) {
+        return runLinearTopologyTest(2, 0, comm);
+    }, TEST_DATA_VALUE, MPI_COMM_WORLD);
 }
 
 TEST(LinearTopol, TestFirstSend) {
-    runLinearTopologyTest(0, 3, MPI_COMM_WORLD);
+    testWithMultipleProcesses([](MPI_Comm comm) {
+        return runLinearTopologyTest(0, 3, comm);
+    }, TEST_DATA_VALUE, MPI_COMM_WORLD);
 }
 
 TEST(LinearTopol, TestLastSend) {
-    runLinearTopologyTest(3, 0, MPI_COMM_WORLD);
+    testWithMultipleProcesses([](MPI_Comm comm) {
+        return runLinearTopologyTest(3, 0, comm);
+    }, TEST_DATA_VALUE, MPI_COMM_WORLD);
 }
 
 TEST(LinearTopol, TestMultipleSends) {
-    runLinearTopologyTest(2, 3, MPI_COMM_WORLD);
+    testWithMultipleProcesses([](MPI_Comm comm) {
+        return runLinearTopologyTest(2, 3, comm);
+    }, TEST_DATA_VALUE, MPI_COMM_WORLD);
 }
 
-TEST(LinearTopol, TestMultipleSends) {
-    runLinearTopologyTest(0, 2, MPI_COMM_WORLD);
+TEST(LinearTopol, TestAnotherMultipleSends) {
+    testWithMultipleProcesses([](MPI_Comm comm) {
+        return runLinearTopologyTest(0, 2, comm);
+    }, TEST_DATA_VALUE, MPI_COMM_WORLD);
 }
 
 int main(int argc, char** argv) {
-    int result_code = 0;
-
     ::testing::InitGoogleTest(&argc, argv);
-    ::testing::TestEventListeners& listeners =
-        ::testing::UnitTest::GetInstance()->listeners();
 
-    if (MPI_Init(&argc, &argv) != MPI_SUCCESS) {
-       std::cerr << "MPI initialization failed" << std::endl;
-       MPI_Abort(MPI_COMM_WORLD, -1);
+    MPI_Init(&argc, &argv);
+
+    int world_rank;
+    MPI_Comm_rank(MPI_COMM_WORLD, &world_rank);
+
+    int result_code;
+    if (world_rank == 0) {
+        result_code = RUN_ALL_TESTS();
     }
 
-    result_code = RUN_ALL_TESTS();
     MPI_Finalize();
 
-    return result_code;
+    if (world_rank != 0) {
+        exit(0);
+    }
+	return result_code;
 }
