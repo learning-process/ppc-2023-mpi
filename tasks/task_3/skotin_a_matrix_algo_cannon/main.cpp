a8015f604a35343d0d919fbcd38bc9accf628d47
Crazy767 <sascha.scotin2011@list.ru>
2023-12-26 14:20:42 +0300

    Fix tests

--- tasks/task_3/skotin_a_matrix_algo_cannon/main.cpp
+++ tasks/task_3/skotin_a_matrix_algo_cannon/main.cpp
@@ -77,9 +77,17 @@ TEST(Matrix_Cannon_Multiply, Test_Large_Matrix) {
     }
 }
 
-int main(int argc, char **argv) {
-    MPI_Init(&argc, &argv);
+int main(int argc, char** argv) {
     ::testing::InitGoogleTest(&argc, argv);
+    MPI_Init(&argc, &argv);
+
+    int rank;
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    auto& listeners = ::testing::UnitTest::GetInstance()->listeners();
+    if (rank != 0) {
+        delete listeners.Release(listeners.default_result_printer());
+    }
+
     int result = RUN_ALL_TESTS();
     MPI_Finalize();
     return result;
