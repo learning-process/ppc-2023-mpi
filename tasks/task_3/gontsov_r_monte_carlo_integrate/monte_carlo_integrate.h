6d80627c4c4506814e0056c10b7b09ba71365e29
FirewallGR <qwert.ting@bk.ru>
2023-12-28 16:29:18 +0300

    changed

--- tasks/task_3/gontsov_r_monte_carlo_integrate/monte_carlo_integrate.h
+++ tasks/task_3/gontsov_r_monte_carlo_integrate/monte_carlo_integrate.h
@@ -6,13 +6,11 @@
 #include <functional>
 
 double getIntegralMonteCarlo(
-    const std::function<double(const std::vector<double>&)>& f,
-    std::vector<double> a, std::vector<double> b, int number_points
-);
+const std::function<double(const std::vector<double>&)>& f,
+ std::vector<double> a, std::vector<double> b, int number_points);
 
-double getIntegralMonteCarloOmp(
+double getIntegralMonteCarloStd(
     const std::function<double(const std::vector<double>&)>& f,
-    std::vector<double> a, std::vector<double> b, int number_points
-);
+    std::vector<double> a, std::vector<double> b, int number_points);
 
 #endif  // TASKS_TASK_3_GONTSOV_R_MONTE_CARLO_INTEGRATE_MONTE_CARLO_INTEGRATE_H_
