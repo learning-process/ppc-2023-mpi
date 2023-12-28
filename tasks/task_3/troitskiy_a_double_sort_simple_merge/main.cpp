c8ef5633e00b073ba3e62b45a5e04ec373fbf115
Alexander Troitskiy <113432533+drinkwr@users.noreply.github.com>
2023-12-28 18:57:15 +0300

    Update main.cpp

--- tasks/task_3/troitskiy_a_double_sort_simple_merge/main.cpp
+++ tasks/task_3/troitskiy_a_double_sort_simple_merge/main.cpp
@@ -4,6 +4,7 @@
 #include <mpi.h>
 #include "./double_radix_sort.h"
 
+
 void print_array(std::vector<double> a, int n) {
     std::cout << "================\n";
     for (int i = 0; i < n; i++) {
@@ -13,121 +14,106 @@ void print_array(std::vector<double> a, int n) {
     std::cout << "================\n";
 }
 
-TEST(MPI_TESTS, Test_size_16) {
-    // assemble
+TEST(MPI_TESTS, Test1) {
     int rank = 0;
-    int proc_count = 0;
+    int countProc = 0;
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-    MPI_Comm_size(MPI_COMM_WORLD, &proc_count);
+    MPI_Comm_size(MPI_COMM_WORLD, &countProc);
     int n = 16;
-    std::vector<double> actual, expected;
+    std::vector<double>a, b;
     if (rank == 0) {
-        actual = generateRandomDoubleVector(n, 1, 100);
-        expected = actual;
-        sort(expected.begin(), expected.end());
+        a = getRandomInput(n, 1, 100);
+        b = a;
+        sort(b.begin(), b.end());
     }
-    // act
-    actual = DoubleRadixSortParallel(actual);
-    // assert
+    a = ParallelRadixSortDouble(a, n);
     if (rank == 0) {
         for (int i = 0; i < n; i++) {
-            ASSERT_EQ(actual[i], expected[i]);
+            ASSERT_EQ(a[i], b[i]);
         }
     }
 }
 
-TEST(MPI_TESTS, Test_size_45) {
-    // assemble
+TEST(MPI_TESTS, Test2) {
     int rank = 0;
-    int proc_count = 0;
+    int countProc = 0;
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-    MPI_Comm_size(MPI_COMM_WORLD, &proc_count);
+    MPI_Comm_size(MPI_COMM_WORLD, &countProc);
     int n = 45;
-    std::vector<double> actual, expected;
+    std::vector<double>a, b;
     if (rank == 0) {
-        actual = generateRandomDoubleVector(n, 1, 100);
-        expected = actual;
-        sort(expected.begin(), expected.end());
+        a = getRandomInput(n, 1, 100);
+        b = a;
+        sort(b.begin(), b.end());
     }
-    // act
-    actual = DoubleRadixSortParallel(actual);
-    // assert
+    a = ParallelRadixSortDouble(a, n);
     if (rank == 0) {
         for (int i = 0; i < n; i++) {
-            ASSERT_EQ(actual[i], expected[i]);
+            ASSERT_EQ(a[i], b[i]);
         }
     }
 }
 
 
-TEST(MPI_TESTS, Test_only_negatives) {
-    // assemble
+TEST(MPI_TESTS, Test_all_neg) {
     int rank = 0;
-    int proc_count = 0;
+    int countProc = 0;
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-    MPI_Comm_size(MPI_COMM_WORLD, &proc_count);
+    MPI_Comm_size(MPI_COMM_WORLD, &countProc);
     int n = 58;
-    std::vector<double> actual, expected;
+    std::vector<double>a, b;
     if (rank == 0) {
-        actual = generateRandomDoubleVector(n, 1, 100);
+        a = getRandomInput(n, 1, 100);
         for (int i = 0; i < n; i++) {
-            actual[i] *= -1;
+            a[i] *= -1;
         }
-        expected = actual;
-        sort(expected.begin(), expected.end());
+        b = a;
+        sort(b.begin(), b.end());
     }
-    // act
-    actual = DoubleRadixSortParallel(actual);
-    // assert
+    a = ParallelRadixSortDouble(a, n);
     if (rank == 0) {
         for (int i = 0; i < n; i++) {
-            ASSERT_EQ(actual[i], expected[i]);
+            ASSERT_EQ(a[i], b[i]);
         }
     }
 }
 
-TEST(MPI_TESTS, Test_size_3) {
-    // assemble
+TEST(MPI_TESTS, Test_small_size) {
     int rank = 0;
-    int proc_count = 0;
+    int countProc = 0;
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-    MPI_Comm_size(MPI_COMM_WORLD, &proc_count);
+    MPI_Comm_size(MPI_COMM_WORLD, &countProc);
     int n = 3;
-    std::vector<double> actual, expected;
+    std::vector<double>a, b;
     if (rank == 0) {
-        actual = generateRandomDoubleVector(n, 1, 100);
-        expected = actual;
-        sort(expected.begin(), expected.end());
+        a = getRandomInput(n, 1, 100);
+        b = a;
+        sort(b.begin(), b.end());
     }
-    // act
-    actual = DoubleRadixSortParallel(actual);
-    // assert
+    a = ParallelRadixSortDouble(a, n);
     if (rank == 0) {
         for (int i = 0; i < n; i++) {
-            ASSERT_EQ(actual[i], expected[i]);
+            ASSERT_EQ(a[i], b[i]);
         }
     }
 }
 
-TEST(MPI_TESTS, Test_size_450) {
-    // assemble
+TEST(MPI_TESTS, Test_big_size) {
     int rank = 0;
-    int proc_count = 0;
+    int countProc = 0;
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-    MPI_Comm_size(MPI_COMM_WORLD, &proc_count);
+    MPI_Comm_size(MPI_COMM_WORLD, &countProc);
     int n = 450;
-    std::vector<double> actual, expected;
+    std::vector<double>a, b;
     if (rank == 0) {
-        actual = generateRandomDoubleVector(n, 1, 100);
-        expected = actual;
-        sort(expected.begin(), expected.end());
+        a = getRandomInput(n, 1, 100);
+        b = a;
+        sort(b.begin(), b.end());
     }
-    // act
-    actual = DoubleRadixSortParallel(actual);
-    // assert
+    a = ParallelRadixSortDouble(a, n);
     if (rank == 0) {
         for (int i = 0; i < n; i++) {
-            ASSERT_EQ(actual[i], expected[i]);
+            ASSERT_EQ(a[i], b[i]);
         }
     }
 }
@@ -135,7 +121,8 @@ TEST(MPI_TESTS, Test_size_450) {
 int main(int argc, char** argv) {
     int result = 0;
     ::testing::InitGoogleTest(&argc, argv);
-    ::testing::TestEventListeners &listeners = ::testing::UnitTest::GetInstance()->listeners();
+    ::testing::TestEventListeners &listeners =
+            ::testing::UnitTest::GetInstance()->listeners();
 
     if (MPI_Init(&argc, &argv) != MPI_SUCCESS)
         MPI_Abort(MPI_COMM_WORLD, -1);
