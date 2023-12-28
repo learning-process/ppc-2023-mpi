f3915974a17005c2af2b65351160d3137fdb6c26
Alexander Troitskiy <113432533+drinkwr@users.noreply.github.com>
2023-12-28 17:36:44 +0300

    Update main.cpp

--- tasks/task_2/troitskiy_a_lin_topology/main.cpp
+++ tasks/task_2/troitskiy_a_lin_topology/main.cpp
@@ -26,27 +26,30 @@ TEST(LinearTopology, Test_procs_num_2) {
     }
 }
 
-// TEST(LinearTopology, Test_descending) {
-//     // assemble
-//     int rank = 0, world_size = 0, data = 0;
+TEST(LinearTopology, Test_descending) {
+    // assemble
+    int rank = 0;
+    int world_size = 0;
+    int data = 0;
 
-//     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-//     MPI_Comm_size(MPI_COMM_WORLD, &world_size);
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    MPI_Comm_size(MPI_COMM_WORLD, &world_size);
 
-//     const int receiving_rank = 0, sending_rank = world_size - 1;
+    const int receiving_rank = 0;
+    const int sending_rank = world_size - 1;
 
-//     if (rank == sending_rank) {
-//         data = 42;
-//     }
+    if (rank == sending_rank) {
+        data = 42;
+    }
 
-//     // act
-//     SendDataLinearTopology(&data, 1, MPI_INT, sending_rank, receiving_rank, 0, MPI_COMM_WORLD);
+    // act
+    SendDataLinearTopology(&data, 1, MPI_INT, sending_rank, receiving_rank, 0, MPI_COMM_WORLD);
 
-//     // assert
-//     if (rank == receiving_rank) {
-//          ASSERT_EQ(42, data);
-//     }
-// }
+    // assert
+    if (rank == receiving_rank) {
+         ASSERT_EQ(42, data);
+    }
+}
 
 TEST(LinearTopology, Test_mid_descending) {
     // assemble
