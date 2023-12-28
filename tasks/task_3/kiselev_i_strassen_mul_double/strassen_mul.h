0fa3ee61f991fe9cf42869a66c3e95f674deab53
ISK7 <105171814+ISK7@users.noreply.github.com>
2023-12-28 23:11:10 +0300

    Киселёв Игорь. Задача3. Вариант3. Алгоритм Штрассена

--- tasks/task_3/kiselev_i_strassen_mul_double/strassen_mul.h
+++ tasks/task_3/kiselev_i_strassen_mul_double/strassen_mul.h
@@ -1,6 +1,6 @@
 // Copyright 2023 Kiselev Igor
-#ifndef TASKS_TASK_3_KISELEV_I_STRASSEN_MUL_STRASSEN_MUL_H_
-#define TASKS_TASK_3_KISELEV_I_STRASSEN_MUL_STRASSEN_MUL_H_
+#ifndef TASKS_TASK_3_KISELEV_I_STRASSEN_MUL_DOUBLE_STRASSEN_MUL_H_
+#define TASKS_TASK_3_KISELEV_I_STRASSEN_MUL_DOUBLE_STRASSEN_MUL_H_
 #include <mpi.h>
 #include <iostream>
 #include <vector>
@@ -18,4 +18,4 @@ std::vector<std::vector<double>> Strassen(const std::vector<std::vector<double>>
 std::vector<std::vector<double>> SeqMulMatrix(const std::vector<std::vector<double>> &SeqA,
     const std::vector<std::vector<double>> &SeqB, int size);
 
-#endif  // TASKS_TASK_3_KISELEV_I_STRASSEN_MUL_STRASSEN_MUL_H_
+#endif  // TASKS_TASK_3_KISELEV_I_STRASSEN_MUL_DOUBLE_STRASSEN_MUL_H_
