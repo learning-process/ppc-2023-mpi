1c7ba5cd0ac63a906ee60cd09339268498ff4ad5
ISK7 <105171814+ISK7@users.noreply.github.com>
2023-12-28 23:59:18 +0300

    Киселёв Игорь. Задача2. Вариант18. Умножение матриц

--- tasks/task_2/kiselev_i_mul_tape_matrix/mul_tape_matrix.h
+++ tasks/task_2/kiselev_i_mul_tape_matrix/mul_tape_matrix.h
@@ -3,10 +3,7 @@
 #define TASKS_TASK_2_KISELEV_I_MUL_TAPE_MATRIX_MUL_TAPE_MATRIX_H_
 #include <mpi.h>
 #include <vector>
-#include <iostream>
-#include <boost/core/invoke_swap.hpp>
 
-
-std::vector<int> SeqMulMatrix(const std::vector<int> &a, const std::vector<int> &b, int n, int m);
-std::vector<int> ParMulMatrix(std::vector<int> *a, std::vector<int> *b, int an, int am);
+std::vector<int> SeqMulMatrix(const std::vector<int>& a, const std::vector<int>& b, int an, int am, int bm);
+std::vector<int> ParMulMatrix(std::vector<int> *SMM, std::vector<int> *PMM, int an, int am);
 #endif  // TASKS_TASK_2_KISELEV_I_MUL_TAPE_MATRIX_MUL_TAPE_MATRIX_H_
