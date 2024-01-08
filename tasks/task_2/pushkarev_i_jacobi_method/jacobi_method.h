9080b5239fca1ac6db454462463fb45917c45dfe
oleg212 <ipnnru@gmail.com>
2024-01-08 15:32:04 +0300

    test

--- tasks/task_2/pushkarev_i_jacobi_method/jacobi_method.h
+++ tasks/task_2/pushkarev_i_jacobi_method/jacobi_method.h
@@ -10,5 +10,6 @@ std::vector<double> Jacobi(const std::vector<double>& A,
     int num_iterations);
 std::vector<double> create_random_matrix(size_t size, double lower_bound, double upper_bound);
 std::vector<double> create_random_vector(size_t size, double lower_bound, double upper_bound);
+
 #endif  // TASKS_TASK_2_PUSHKAREV_I_JACOBI_METHOD_JACOBI_METHOD_H_
 
