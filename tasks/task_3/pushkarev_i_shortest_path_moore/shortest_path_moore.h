d01453c821626fa7e55188f389e213927f8992dc
oleg212 <ipnnru@gmail.com>
2024-01-09 18:01:40 +0300

    vector reference

--- tasks/task_3/pushkarev_i_shortest_path_moore/shortest_path_moore.h
+++ tasks/task_3/pushkarev_i_shortest_path_moore/shortest_path_moore.h
@@ -7,6 +7,6 @@
 
 const int INF = INT_MAX / 2;
 
-std::vector<int> mooore(int my_rank, int p, int n, std::vector<int> mat, bool* has_negative_cycle);
+std::vector<int> mooore(int my_rank, int p, int n, std::vector<int>& mat, bool* has_negative_cycle);
 #endif  // TASKS_TASK_3_PUSHKAREV_I_SHORTEST_PATH_MOORE_SHORTEST_PATH_MOORE_H_
 
