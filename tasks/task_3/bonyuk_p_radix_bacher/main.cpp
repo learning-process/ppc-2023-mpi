87ae2daf298b82d9b03c04628e61e9d9842360d0
PetrBonyuk <152027126+PetrBonyuk@users.noreply.github.com>
2024-01-10 17:22:45 +0300

    1234оорkld

--- tasks/task_3/bonyuk_p_radix_bacher/main.cpp
+++ tasks/task_3/bonyuk_p_radix_bacher/main.cpp
@@ -6,38 +6,68 @@
 #include "task_3/bonyuk_p_radix_bacher/radix_bacher.h"
 
 TEST(RadixSortTest, PositiveTest) {
-    std::vector<int> v = { 10, 7, 3, 1, 9, 5, 4, 8 };
-    std::vector<int> const expected_v = { 1, 3, 4, 5, 7, 8, 9, 10 };
+    std::vector<int> v(180);
+
+    std::srand(std::time(nullptr));
+    for (int i = 0; i < 180; ++i) {
+        v[i] = std::rand() % 2001;
+    }
     v = radixSort(v);
-    EXPECT_EQ(v, expected_v);
+    for (int i = 0; i < static_cast<int>(v.size()) - 1; i++) {
+        EXPECT_TRUE(v[i] <= v[i + 1]);
+    }
 }
 
 TEST(RadixSortTest, NegativeTest) {
-    std::vector<int> v = { -1, -3, -7, -2, -4, -12, -5, -10};
-    std::vector<int> const expected_v = { -12, -10, -7, -5, -4, -3, -2, -1 };
+    std::vector<int> v(180);
+
+    std::srand(std::time(nullptr));
+    for (int i = 0; i < 180; ++i) {
+         v[i] = std::rand() % 2001 - 2001;
+    }
     v = radixSort(v);
-    EXPECT_EQ(v, expected_v);
+    for (int i = 0; i < static_cast<int>(v.size()) - 1; i++) {
+        EXPECT_TRUE(v[i] <= v[i + 1]);
+    }
 }
 
 TEST(RadixSortTest, RepeatTest) {
-    std::vector<int> v = { 10, 7, 3, 1, 9, 7, 9, 8 };
-    std::vector<int> const expected_v = { 1, 3, 7, 7, 8, 9, 9, 10 };
+    std::vector<int> v(180);
+
+    std::srand(std::time(nullptr));
+    for (int i = 0; i < 180; ++i) {
+        v[i] = std::rand() % 10;
+    }
     v = radixSort(v);
-    EXPECT_EQ(v, expected_v);
+    for (int i = 0; i < static_cast<int>(v.size()) - 1; i++) {
+        EXPECT_TRUE(v[i] <= v[i + 1]);
+    }
 }
 
-TEST(RadixSortTest, EmptyVectorTest) {
-    std::vector<int> v;
-    std::vector<int> const expected_v;
+TEST(RadixSortTest, VectorTest) {
+    std::vector<int> v(180);
+
+    std::srand(std::time(nullptr));
+    for (int i = 0; i < 180; ++i) {
+        v[i] = std::rand() % 201;
+    }
     v = radixSort(v);
-    EXPECT_EQ(v, expected_v);
+    for (int i = 0; i < static_cast<int>(v.size()) - 1; i++) {
+        EXPECT_TRUE(v[i] <= v[i + 1]);
+    }
 }
 
 TEST(RadixSortTest, MixedPositiveAndNegativeTest) {
-    std::vector<int> v = { -2, 3, -1, 5, -4, 7, 6, -8 };
-    std::vector<int> const expected_v = { -8, -4, -2, -1, 3, 5, 6, 7 };
+    std::vector<int> v(180);
+
+    std::srand(std::time(nullptr));
+    for (int i = 0; i < 180; ++i) {
+        v[i] = std::rand() % 2001 - 1000;
+    }
     v = radixSort(v);
-    EXPECT_EQ(v, expected_v);
+    for (int i = 0; i < static_cast<int>(v.size()) - 1; i++) {
+        EXPECT_TRUE(v[i] <= v[i + 1]);
+    }
 }
 
 int main(int argc, char **argv) {
