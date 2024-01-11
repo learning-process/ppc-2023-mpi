a7130904650b9aa5ef956741283576e2a334c754
ks2k23 <lbdvks@mail.ru>
2024-01-11 04:38:42 +0300

    task1_done2

--- tasks/task_1/lebedeva_k_words_count/words_count.h
+++ tasks/task_1/lebedeva_k_words_count/words_count.h
@@ -1,6 +1,6 @@
 // Copyright 2023 Lebedeva Ksenia
-#ifndef TASKS_TASK_1_LEBEDEVA_K_WORDS_COUNT_H_
-#define TASKS_TASK_1_LEBEDEVA_K_WORDS_COUNT_H_
+#ifndef TASKS_TASK_1_LEBEDEVA_K_WORDS_COUNT_WORDS_COUNT_H_
+#define TASKS_TASK_1_LEBEDEVA_K_WORDS_COUNT_WORDS_COUNT_H_
 
 #include <stdlib.h>
 #include <mpi.h>
@@ -12,4 +12,4 @@ int getWordsCountParallel(const std::string& str, int size);
 int getWordsCountFragment(const std::string& str, int size);
 int getWordsCountSequentially(const std::string& str, int size);
 
-#endif  // TASKS_TASK_1_LEBEDEVA_K_WORDS_COUNT_H_
+#endif  // TASKS_TASK_1_LEBEDEVA_K_WORDS_COUNT_WORDS_COUNT_H_
