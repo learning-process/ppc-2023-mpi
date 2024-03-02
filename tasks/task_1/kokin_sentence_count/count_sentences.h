350e006ee3bf40ddb5ff0735eef4aa87f62b6ecc
ThreeToads <ivan.kokin.465@gmail.com>
2024-03-03 00:43:07 +0300

    add task_1

---
+++ tasks/task_1/kokin_sentence_count/count_sentences.h
@@ -0,0 +1,10 @@
+// Copyright 2024 Kokin Ivan
+
+#ifndef TASKS_TASK_1_KOKIN_SENTENCE_COUNT_COUNT_SENTENCES_H_
+#define TASKS_TASK_1_KOKIN_SENTENCE_COUNT_COUNT_SENTENCES_H_
+
+#include <string>
+
+int countSentences(const std::string& input);
+int countFunc(const std::string& input);
+#endif  // TASKS_TASK_1_KOKIN_SENTENCE_COUNT_COUNT_SENTENCES_H_
