cce8fbfe457bbeaaa8191086eb26c086e49c7cbe
KirFedotov <k.fedotov.guit29@mail.ru>
2024-03-09 17:32:54 +0300

    rel5(fin)

--- tasks/task_2/fedotov_k_gather/gather.h
+++ tasks/task_2/fedotov_k_gather/gather.h
@@ -7,5 +7,5 @@
 int MPI_Gather_c(const void *sendbuf, int sendcount, MPI_Datatype sendtype,
                  void *recvbuf, int recvcount, MPI_Datatype recvtype,
                  int root, MPI_Comm comm);
-                 
+
 #endif  // TASKS_TASK_2_FEDOTOV_K_GATHER_GATHER_H_
